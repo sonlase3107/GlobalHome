7f4bbdfc4833d71ab5f578bf09ea4d8f04d9430e
LAS <sonlase3107@gmail.com>
2024-10-31 21:31:40 +0700

    add

--- Outbox/obj/Debug/net8.0/Outbox.AssemblyInfo.cs
+++ Outbox/obj/Debug/net8.0/Outbox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Outbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12b22a104a0f91f5128da0fea7a3e90e4126485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c096b8d48bca8648f81b39968c645d78440f4e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Outbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Outbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
